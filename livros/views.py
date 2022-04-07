0bf15bc8058c04943f2f935584e8f0ae3843c913
Jose Mauricio <grandemau@gmail.com>
2022-04-06 21:14:11 -0300

    Adicionado modls, urls e templates

--- livros/views.py
+++ livros/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Livros 
 
 # Create your views here.
+
+def index(request):
+    template_name = "livros/base.html"
+    return render(request, template_name)
