d5ff24fb97f5c3cdc1ccb4a6a0faef63c89b3735
Jose Mauricio <grandemau@gmail.com>
2022-04-11 17:51:17 -0300

    Adicionado generic views

--- livros/models.py
+++ livros/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Livros(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.CharField(max_length=400)
     editora = models.CharField(max_length=200)
     quantidade = models.IntegerField()
     data_cadastro = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.titulo
 
