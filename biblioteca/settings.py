aec1dfddf0d3fa736580d5e33bfcf4de4482ec72
Jose Mauricio <grandemau@gmail.com>
2022-04-11 21:15:03 -0300

    Adicionado configuração bd postgres

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'biblioteca',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- livros/views.py
+++ livros/views.py
@@ -10,46 +10,46 @@ from .models import Livros
 # Create your views here.
 
 
-def index(request):
-    livros = Livros.objects.all().order_by('-data_cadastro')
-    return render(request, "livros/listar.html", {'livros': livros})
+# def index(request):
+#     livros = Livros.objects.all().order_by('-data_cadastro')
+#     return render(request, "livros/listar.html", {'livros': livros})
 
 
-def visualizar(request, id):
-    livro = get_object_or_404(Livros, pk=id)
-    return render(request, "livros/visualizar.html", {'livro': livro})
+# def visualizar(request, id):
+#     livro = get_object_or_404(Livros, pk=id)
+#     return render(request, "livros/visualizar.html", {'livro': livro})
 
 
-def cadastrar(request):
-    if request.method == 'POST':
-        form = LivrosForm(request.POST)
-        if form.is_valid():
-            livro = form.save()
-            return redirect('/')
-    else:
-        form = LivrosForm()
-    return render(request, "livros/cadastrar.html", {'form': form})
+# def cadastrar(request):
+#     if request.method == 'POST':
+#         form = LivrosForm(request.POST)
+#         if form.is_valid():
+#             livro = form.save()
+#             return redirect('/')
+#     else:
+#         form = LivrosForm()
+#     return render(request, "livros/cadastrar.html", {'form': form})
 
 
-def atualizar(request, id):
-    livro = get_object_or_404(Livros, pk=id)
-    form = LivrosForm(instance=livro)
-    if(request.method == 'POST'):
-        form = LivrosForm(request.POST, instance=livro)
-        if(form.is_valid()):
-            livro.save()
-            return redirect('/')
-        else:
-            return render(request, "livros/atualizar.html", {'form': form, 'livro': livro})
-    else:
-        return render(request, "livros/atualizar.html", {'form': form, 'livro': livro})
+# def atualizar(request, id):
+#     livro = get_object_or_404(Livros, pk=id)
+#     form = LivrosForm(instance=livro)
+#     if(request.method == 'POST'):
+#         form = LivrosForm(request.POST, instance=livro)
+#         if(form.is_valid()):
+#             livro.save()
+#             return redirect('/')
+#         else:
+#             return render(request, "livros/atualizar.html", {'form': form, 'livro': livro})
+#     else:
+#         return render(request, "livros/atualizar.html", {'form': form, 'livro': livro})
 
 
-def deletar(request, id):
-    produto = get_object_or_404(Livros, pk=id)
-    produto.delete()
-    messages.success(request, 'Livro deletado')
-    return redirect('/')
+# def deletar(request, id):
+#     produto = get_object_or_404(Livros, pk=id)
+#     produto.delete()
+#     messages.success(request, 'Livro deletado')
+#     return redirect('/')
 
 # def pesquisar(request, texto):
 #     livros = Livros.objects.filter(titulo__like="texto")
@@ -60,6 +60,7 @@ def deletar(request, id):
 
 class LivrosDetailView(DetailView):
     model = Livros
+    context_object_name = 'livro'
     template_name = 'livros/visualizar.html'
 
 
@@ -67,6 +68,14 @@ class IndexView(generic.ListView):
     template_name = 'livros/listar.html'
     context_object_name = 'livros'
     model = Livros
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     query = self.request.GET.get('q', '')
+    #     list_user = Livros.objects.filter(titulo__icontains=query) if query != '' else Livros.objects.all()
+
+    #     context['livros'] = query
+    #     return context
 
 
 class LivrosCreateView(CreateView):
